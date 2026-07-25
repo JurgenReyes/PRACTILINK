81f6ff21f69f838c72e4c815d37abb6deab56875
JurgenReyes <up230538@alumnos.upa.edu.mx>
2026-07-25 00:00:06 -0600

    update: gestion de vacantes actualizada (V4)

--- backend/src/routes/vacanteRoutes.js
+++ backend/src/routes/vacanteRoutes.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const { wrapRouter } = require("../middleware/asyncHandler");
+const router = wrapRouter(require("express").Router());
 const vacantes = require("../controllers/vacanteController");
 const { requiereAutenticacion, requiereRol } = require("../middleware/auth");
 
